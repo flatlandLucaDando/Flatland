2018bb4bb1357d97128ba0760826ae6d99e21140
LucaFronda <aquilarealecoku@hotmail.it>
2021-08-23 23:50:17 +0200

    Update structures.py
    Changes in the train timetable

--- structures.py
+++ structures.py
@@ -38,23 +38,23 @@ railway_example_1 = [[(0, 0),  (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0,0), (0
 station_a = (21, 1)
 station_b = (21, 37)
 station_c = (8, 14)
-stationo_d = (1, 36)
+station_d = (1, 36)
 station_e = (15, 49)
 
-# An array more compact to store the different stations
-
-# TODO Modify the stations, add a number to indicate the type of line in which the station is. If high velocity or regional...
-stations = [station_a, station_b, station_c, stationo_d, station_e]
+# An array more compact to store the different stations, for each station the array conteins the position (x,y) of the station, the maximum
+# velocity supported by the line (high velocity, regional...)
+stations = [[station_a, 1.], [station_b, 1.], [station_c, 0.5], [station_d, 0.5], [station_e, 1.]]
 
 # Timetable conteins the station where the train should pass, from starting station to aim, and conteins the time at which
 # each train has to pass in the station, the last number represent the velocity of train (high velocity, intercity or regional)
 # Each row represent a different train
 
 timetable_example_1 = [[(station_a, station_b, station_e), (10 ,50, 80), (1.)],   		 # agent 0   high velocity
-			 [(station_b, station_a),(0, 38), (1. / 2.)],                     # agent 1   Intercity
-             [(station_c, station_b), (30, 109), (1.)],                  	# agent 2    High velocity
-             [(stationo_d, station_e), (30, 98), (1. / 4.)],                 # agent 3   Regional
-             [(station_e, stationo_d), (50, 107), (1. / 4.)]]                 # agent 4   Regional
+			 [(station_b, station_a),(0, 38), (1. / 2.)],                     			 # agent 1   Intercity
+             [(station_c, station_b), (30, 109), (1.)],                  	             # agent 2   High velocity
+             [(station_d, station_e), (30, 98), (1. / 4.)],                             # agent 3   Regional
+             [(station_e, station_d), (50, 107), (1. / 4.)],					         # agent 4   Regional
+             [(station_d, station_c), (60, 120), (1. / 2.)]]                              # agent 5   Intercity
 
 
 # Different velocities in different lines.
