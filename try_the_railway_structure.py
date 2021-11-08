98801d611e9ce5ac565bd542bcb4b597f8936598
LucaFronda <aquilarealecoku@hotmail.it>
2021-11-08 18:20:27 +0100

    File to try the structure of the railway

---
+++ try_the_railway_structure.py
@@ -0,0 +1,91 @@
+import time
+import numpy as np
+import os
+# In Flatland you can use custom observation builders and predicitors
+# Observation builders generate the observation needed by the controller
+# Preditctors can be used to do short time prediction which can help in avoiding conflicts in the network
+from flatland.envs.malfunction_generators import malfunction_from_params, MalfunctionParameters, ParamMalfunctionGen
+from flatland.envs.observations import TreeObsForRailEnv, GlobalObsForRailEnv
+# First of all we import the Flatland rail environment
+from flatland.envs.rail_env import RailEnv
+from flatland.envs.rail_env import RailEnvActions
+# Import the railway generators
+from flatland.envs.custom_rail_generator import rail_custom_generator
+from flatland.utils.rendertools import RenderTool, AgentRenderVariant
+# Import the schedule generators
+from flatland.envs.custom_schedule_generator import custom_schedule_generator
+from flatland.envs.plan_to_follow_utils import action_to_do, divide_trains_in_station_rails, control_timetable
+# Import the different structures needed
+from configuration import railway_example, stations, timetable_example
+# Import the agent class
+from flatland.envs.agent import RandomAgent
+
+
+
+# Flag active in case of interruptions
+interruption = False
+
+# The specs for the custom railway generation are taken from structures.py file
+specs = railway_example
+
+widht = len(specs[0])
+height = len(specs)
+
+stations_position = []
+
+# Defining the name of the different stations
+for i in range(1, len(stations)):
+    stations_position.append(stations[i][0])
+
+# Timetable conteins the station where the train should pass, from starting station to aim, and conteins the time at which
+# each train has to pass in the station, the last number represent the velocity of train (high velocity, intercity or regional)
+# Each row represent a different train
+
+print('------ Calculating the timetable')
+print()
+timetable = timetable_example
+
+#divide_trains_in_station_rails(timetable)
+
+for i in range(len(timetable)):
+    print(timetable[i])
+
+# Number of agents is the rows of the timetable
+num_of_agents = len(timetable)
+
+# Check if the timetable is feaseble or not, the function is in schedule_generators
+# A timetable is feaseble if the difference of times between two stations is positive and let the trains to reach the successive station
+# if two stations are very distant from each other the difference of times can't be very small
+seed = 2
+
+# Generating the railway topology, with stations
+# Arguments of the generator (specs of the railway, position of stations, timetable)
+rail_custom = rail_custom_generator(specs, stations_position, timetable)
+
+transition_map_example, agent_hints = rail_custom(widht, height, num_of_agents)
+
+# We can now initiate the schedule generator with the given speed profiles
+schedule_generator_custom = custom_schedule_generator(timetable = timetable)
+
+
+
+TreeObservation = GlobalObsForRailEnv()
+
+env = RailEnv(  width= widht,
+                height= height,
+                rail_generator = rail_custom,
+                line_generator=schedule_generator_custom,
+                number_of_agents= num_of_agents,
+                obs_builder_object=TreeObservation,
+                remove_agents_at_target=True,
+                record_steps=True
+                )
+
+
+env.reset()
+
+env_renderer = RenderTool(env)
+env_renderer.render_env(show=True, show_predictions=False, show_observations=False)
+
+time.sleep(5)
+
